6c49984af019a417cbcc19cd88a130603a65edc2
daikagoku <80370767+daikagoku@users.noreply.github.com>
2022-05-06 14:41:54 +0700

    update 06/06

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import './App.css';
 
-import {useEffect,Component} from "react";
+import {useEffect} from "react";
 import {Routes,Route}from'react-router-dom';
 import AppProvider from "./AppProvider";
 
-import UserPage from "./Screens/User/";
-import AdminPage from "./Screens/Admin/";
+import Auth from "./Screens/Auth/";
 import LoadingData from "./Screens/LoadingData/";
 import Toasts from "./Screens/Toasts/";
-import Auth from "./Screens/Auth/";
-
+import UserPage from "./Screens/User/";
+import AdminPage from "./Screens/Admin/";
+import {getAreaName} from "./Config/Route/";
 
 function App() {
   useEffect(function(){
     global.config.setTitleWebsite("");
   },[]);
-  const {getAreaName} = global.config.useRoute();
   return (
       <AppProvider>
         <LoadingData />
         <Toasts />
         <Auth />
         <Routes>
-          <Route path={`${getAreaName("user")}/*`} element={<UserPage />} />
-          <Route path={`${getAreaName("admin")}*`} element={<AdminPage />} />
-        </Routes>
+          <Route path={`${getAreaName("user")}/*`} element={<UserPage />} /> 
+          <Route path={`${getAreaName("admin")}*`} element={<AdminPage />} />     
+        </Routes>  
       </AppProvider>
   );
 }
 
 export default App;
+/*
+
+
+
+
+        
+        
+      
+        
+*/
