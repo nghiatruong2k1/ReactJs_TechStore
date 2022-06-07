bddb17537daec890de91f152a6aeee2feb997493
daikagoku <80370767+daikagoku@users.noreply.github.com>
2022-06-07 16:22:53 +0700

    update

--- src/Screens/User/Content/Cart/index.js
+++ src/Screens/User/Content/Cart/index.js
@@ -1,14 +1,15 @@
-import {memo,Fragment} from 'react';
+import {memo,Fragment,useEffect} from 'react';
 
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route,useLocation} from 'react-router-dom';
 import {getActionName} from "../../../../Config/Route/";
 
 import HomeCart from './Home/';
 import ConfirmCart from './Confirm/';
 function Component(props){
+    const location = useLocation()
     useEffect(function(){
         global.config.setTitleWebsite("Giỏ hàng");
-    })
+    },[location])
     return (
         <Fragment>
             <HomeCart />
--- src/Screens/User/Content/Home/index.js
+++ src/Screens/User/Content/Home/index.js
@@ -1,14 +1,15 @@
 import {memo,useEffect} from 'react';
-
+import { useLocation } from 'react-router-dom';
 import AsideTop from "./AsideTop/";
 import DealsOffers from "./DealsOffers/";
 import Brands from "./Brands/";
 import Recommend from "./Recommend/";
 
 function HomeContent({title,...props}){
+  const location = useLocation();
   useEffect(function(){
     global.config.setTitleWebsite(title);
-  })
+  },[location])
   return(
     <>
       <AsideTop />
